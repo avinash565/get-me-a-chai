9d34505a1a71c4cdd99403dba9ff2282dfb8c5fc
Avinash Kumar Yadav <avinashkumar2069l@gmail.com>
2026-09-16 04:39:28 +0530

    Connect MongoDB using environment variable

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb://localhost:27017/GetMeAChai`);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
